ab2ea4556fa28e0b9191d337a42657a40df0493a
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 07:18:47 +0530

    Update cust_filters_sql.py

--- Sophia/modules/sql/cust_filters_sql.py
+++ Sophia/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from HexzyBot.modules.helper_funcs.msg_types import Types
-from HexzyBot.modules.sql import BASE, SESSION
+from Sophia.modules.helper_funcs.msg_types import Types
+from Sophia.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
