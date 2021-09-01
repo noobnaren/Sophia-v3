8e0b86bc1df96d3175360b79d81ed0ae0fc2e087
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 14:38:22 +0530

    Update __forcesubs.py

--- Sophia/modules/__forcesubs.py
+++ Sophia/modules/__forcesubs.py
@@ -180,21 +180,19 @@ def config(client, message):
 
 
 __help__ = """
-*Force Subscribe:*
-❍ Sophia can mute members who are not subscribed your channel until they subscribe
-❍ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
-
+*ForceSubscribe:*
+- Sophia can mute members who are not subscribed your channel until they subscribe
+- When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 *Setup*
-*Only creator*
-❍ Add me in your group as admin
-❍ Add me in your channel as admin 
- 
+1) First of all add me in the group as admin with ban users permission and in the channel as admin.
+Note: Only creator of the group can setup me and i will not allow force subscribe again if not done so.
+
 *Commmands*
- ❍ /fsub {channel username} - To turn on and setup the channel.
-  - Do this first...
- ❍ /fsub - To get the current settings.
- ❍ /fsub disable - To turn of ForceSubscribe..
-  - If you disable fsub, you need to set again for working.. /fsub {channel username} 
- ❍ /fsub clear - To unmute all members who muted by me.
+ - /fsub {channel username} - To turn on and setup the channel.
+    Do this first...
+ - /fsub - To get the current settings.
+ - /fsub disable - To turn of ForceSubscribe..
+    If you disable fsub, you need to set again for working.. /fsub {channel username} 
+ - /fsub clear - To unmute all members who muted by me.
 """
-__mod_name__ = "F-Sub "
+__mod_name__ = "Force Subscribe"
