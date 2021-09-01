e08b51269f37e29a064a00644dc076d085b36fef
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 06:59:25 +0530

    Update chat_status.py

--- Sophia/modules/helper_funcs/chat_status.py
+++ Sophia/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from HexzyBot import (
+from Sophia import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from HexzyBot.modules import connection
+from Sophia.modules import connection
 
 connected = connection.connected
