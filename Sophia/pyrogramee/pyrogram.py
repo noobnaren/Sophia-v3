6cbff13a310585792c1ab3b8ae42040bea1eddbc
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 07:33:17 +0530

    Update pyrogram.py

--- Sophia/pyrogramee/pyrogram.py
+++ Sophia/pyrogramee/pyrogram.py
@@ -3,13 +3,13 @@ import logging
 from pyrogram import Client
 
 # from pyromod import listen
-from HexzyBot.conf import get_int_key, get_str_key
+from Sophia.conf import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 API_ID = get_int_key("API_ID", required=True)
 API_HASH = get_str_key("API_HASH", required=True)
 session_name = TOKEN.split(":")[0]
-Hxy = Client(
+Sophia = Client(
     session_name,
     api_id=API_ID,
     api_hash=API_HASH,
@@ -19,4 +19,4 @@ Hxy = Client(
 # disable logging for pyrogram [not for ERROR logging]
 logging.getLogger("pyrogram").setLevel(level=logging.ERROR)
 
-Hxy.start()
+Sophia.start()
