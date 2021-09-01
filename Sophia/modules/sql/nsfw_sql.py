8daac8cad38487e4fc18ff4c0a92990ec578a2e6
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 07:23:17 +0530

    Update nsfw_sql.py

--- Sophia/modules/sql/nsfw_sql.py
+++ Sophia/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from HexzyBot.modules.sql import BASE, SESSION
+from Sophia.modules.sql import BASE, SESSION
 
 class NSFWChats(BASE):
     __tablename__ = "nsfw_chats"
