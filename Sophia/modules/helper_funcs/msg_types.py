91b7b1d1868622833a9346afdcc3508edb3038eb
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 07:05:59 +0530

    Update msg_types.py

--- Sophia/modules/helper_funcs/msg_types.py
+++ Sophia/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from HexzyBot.modules.helper_funcs.string_handling import button_markdown_parser
+from Sophia.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
