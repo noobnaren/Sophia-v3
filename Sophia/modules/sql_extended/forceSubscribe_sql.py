82c7ccd3b5c1ea978405b1dbb1bd3216033db6b1
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 07:29:28 +0530

    Update forceSubscribe_sql.py

--- Sophia/modules/sql_extended/forceSubscribe_sql.py
+++ Sophia/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from HexzyBot.modules.sql import BASE, SESSION
+from Sophia.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
