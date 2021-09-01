e2a70a1f8be56aef81bf17666a022a565784514d
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 07:31:36 +0530

    Update errors.py

--- Sophia/pyrogramee/errors.py
+++ Sophia/pyrogramee/errors.py
@@ -1,7 +1,7 @@
 import sys
 import traceback
 from functools import wraps
-from HexzyBot import pbot, SUPPORT_CHAT
+from Sophia import pbot, SUPPORT_CHAT
 
 def split_limits(text):
     if len(text) < 2048:
